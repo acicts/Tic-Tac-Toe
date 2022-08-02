6deb80272a9bc10f9470cc549164f660e0ba843e
shakyapeiris <shakyaimanjith32@gmail.com>
2022-08-02 14:26:25 +0530

    Refactor and comment code

--- client/app.js
+++ client/app.js
@@ -1,10 +1,34 @@
+// DOM Nodes
+const startingWindow = document.getElementById("starting-window");
+const gameWindow = document.getElementById("game-window");
+const startBtn = document.getElementById("start-btn");
+const resetBtn = document.getElementById("reset-btn");
+const messageBox = document.getElementById("message")
+
+// Current Player and Status of the game
+let currPlayer = "X";
+let playStatus = "";
+
+// Current board
 let board = [
     ["", "", ""],
     ["", "", ""],
     ["", "", ""],
 ]
 
+// Ids of buttons in the board
+const btnIds = [
+    "1-1", "1-2", "1-3",
+    "2-1", "2-2", "2-3",
+    "3-1","3-2","3-3",
+]
+
+// Logic to check the winnner
+// This function will be executed per each press 
+// on a equare  on the board
 const checkWinner = () => {
+
+    // Checking rows
     for (let i=0; i<3; i++){
         const a = board[i][0];
         const b = board[i][1];
@@ -15,6 +39,7 @@ const checkWinner = () => {
         }
     }
 
+    // Checking columns
     for (let i=0; i<3; i++){
         const a = board[0][i];
         const b = board[1][i];
@@ -25,6 +50,7 @@ const checkWinner = () => {
         }
     }
     
+    // Left Top to Bottom right diagonal
     const a = board[0][0];
     const b = board[1][1];
     const c = board[2][2];
@@ -33,6 +59,7 @@ const checkWinner = () => {
         return "win"
     }
 
+    // Right Top to Left bottom diagonal
     const d = board[0][2];
     const e = board[1][1];
     const f = board[2][0];
@@ -41,6 +68,9 @@ const checkWinner = () => {
         return "win"
     }
 
+    // Check for draw
+    // The game is considered as a draw once all the squares
+    // consiste of either 'X' or 'O'
     for (let i=0; i<3; i++){
         for (let j=0; j<3; j++){
             const square = board[i][j];
@@ -51,45 +81,19 @@ const checkWinner = () => {
     return "draw";
 }
 
-let currPlayer = "X";
-const playStatus = "";
-
-const startingWindow = document.getElementById("starting-window");
-const gameWindow = document.getElementById("game-window");
-const startBtn = document.getElementById("start-btn");
-const resetBtn = document.getElementById("reset-btn");
-
-
+// Display only the start button in the start
 window.addEventListener("load", () => {
     gameWindow.style.display = "none";
     resetBtn.style.display = "none";
 })
 
+// Open game window once the start button is clicked
 startBtn.addEventListener("click", () => {
     gameWindow.style.display = "block";
     startingWindow.style.display = "none";
 })
 
-resetBtn.addEventListener("click", () => {
-    board = [
-        ["", "", ""],
-        ["", "", ""],
-        ["", "", ""],
-    ];
-})
-
-const btnIds = [
-    "1-1",
-    "1-2",
-    "1-3",
-    "2-1",
-    "2-2",
-    "2-3",
-    "3-1",
-    "3-2",
-    "3-3",
-]
-
+// Reset board after the game is finished
 resetBtn.addEventListener("click", () => {
     board = [
         ["", "", ""],
@@ -105,8 +109,7 @@ resetBtn.addEventListener("click", () => {
 
 })
 
-const messageBox = document.getElementById("message")
-
+// Listening to clicks on the buttons in the board
 btnIds.forEach((btn) => {
     const button = document.getElementById(btn);
     
@@ -114,7 +117,8 @@ btnIds.forEach((btn) => {
         const coordinate = btn.split("-")
         const y = parseInt(coordinate[0]) - 1;
         const x = parseInt(coordinate[1]) - 1;
-        console.log(board)
+
+        // Check whether the selected button is empty
         if (board[y][x] === ""){
             button.innerHTML = currPlayer;
             board[y][x] = currPlayer;
@@ -141,12 +145,3 @@ btnIds.forEach((btn) => {
     })
 })
 
-square.addEventListener("click", (e) => {
-    console.log(e);
-})
-
-const startPlay = () => {}
-
-const playMove = (y, x, sign) => {
-
-}
